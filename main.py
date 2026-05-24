852a2a39e903d8acb7b0643700e7b18630bef40f
princepia161 <42060271+princepia161@users.noreply.github.com>
2026-05-24 12:08:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from app import generate_drm_keys
 
 API_ID = 20807000
 API_HASH = 'cde2366a7c61e23f4cb44618cbe6cf70'
-BOT_TOKEN = '8651546298:AAHVdnjJi1MNojVj6GmFbNWsEoKTVnOlGcU'
+BOT_TOKEN = '7686849126:AAFjtFz6YZlLP-FnadMvrFTJIsphVr1OYEY'
 OWNER_ID = [5938871512, 890749443] 
 
 bot = Client("ProDownloader", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, in_memory=True)
