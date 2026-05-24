0b6a26362e0e0bf5f4342e9951d8178979509518
princepia161 <42060271+princepia161@users.noreply.github.com>
2026-05-24 12:13:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # ===== BOT SETUP =====
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7686849126:AAFjtFz6YZlLP-FnadMvrFTJIsphVr1OYEY")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8564398983:AAGxMpPkmLcgZsPnVzIQzCUIro5KNk76QBw")
 API_ID = int(os.environ.get("API_ID", "20807000")) # 12345678 की जगह अपनी ID डालें
 API_HASH = os.environ.get("API_HASH", "cde2366a7c61e23f4cb44618cbe6cf70")
 OWNER_ID = [int(os.environ.get("OWNER_ID", "890749443"))]
